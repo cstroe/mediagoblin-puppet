9c267de7a8834614c108d913716e32c0c1adb065
Cosmin Stroe <cstroe@users.sf.net>
2016-01-07 01:54:17 -0500

    Bring in Node.js.

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,4 +2,8 @@ class mediagoblin {
   package { ['python-lxml', 'python-imaging', 'python-virtualenv']:
     ensure => installed,
   }
+
+  package { ['nodejs-legacy', 'npm']:
+    ensure => installed,
+  }
 }
