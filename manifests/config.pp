00e4300e40ba8baf7738ff7165773b09597d612d
Cosmin Stroe <cstroe@users.sf.net>
2016-01-11 15:54:07 -0500

    Add a local ini file.

---
+++ manifests/config.pp
@@ -0,0 +1,15 @@
+class mediagoblin::config {
+  file { 'mediagoblin_local_config':
+    ensure  => 'present',
+    path    => "${::mediagoblin::install_path}/mediagoblin_local.ini",
+    content => 'puppet:///mediagoblin/mediagoblin_local.ini',
+    notify  => Exec['mediagoblin_update_database'],
+  }
+
+  exec { 'mediagoblin_update_database':
+    command => "${::mediagoblin::install_path}/bin/gmg dbupdate",
+    cwd     => $::mediagoblin::install_path,
+    user    => 'mediagoblin',
+    path    => ['/bin', '/usr/bin'],
+  }
+}
