a8c827e58dafa547aca7df1bfab01980dbaf0f4b
Cosmin Stroe <cstroe@users.sf.net>
2016-01-11 17:02:38 -0500

    Add an Nginx template for Mediagoblin.

---
+++ manifests/nginx.pp
@@ -0,0 +1,12 @@
+class mediagoblin::nginx(
+  $nginx_dir                = '/etc/nginx',
+  $domain                   = 'media.onawh.im',
+  $mediagoblin_install_path = $::mediagoblin::install_path,
+) {
+
+  file { 'mediagoblin_nginx_config':
+    ensure  => 'present',
+    path    => "${nginx_dir}/conf.d/mediagoblin.conf",
+    content => template('puppet:///mediagoblin/mediagoblin.conf.erb'),
+  }
+}
