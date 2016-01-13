9f308fa5ad30dfd667ffcfb0b1492b4ebf146a7f
Cosmin Stroe <cstroe@users.sf.net>
2016-01-13 17:31:18 -0500

    Don't run a database update. Leave this to be a manual update.

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,14 +3,14 @@ class mediagoblin::config {
     ensure  => 'present',
     path    => "${::mediagoblin::install_path}/mediagoblin_local.ini",
     content => 'puppet:///mediagoblin/mediagoblin_local.ini',
-    notify  => Exec['mediagoblin_update_database'],
+#    notify  => Exec['mediagoblin_update_database'],
   }
 
-  exec { 'mediagoblin_update_database':
-    command     => "${::mediagoblin::install_path}/bin/gmg dbupdate",
-    cwd         => $::mediagoblin::install_path,
-    environment => "HOME=${::mediagoblin::homedir_path}",
-    user        => 'mediagoblin',
-    path        => ['/bin', '/usr/bin'],
-  }
+#  exec { 'mediagoblin_update_database':
+#    command     => "${::mediagoblin::install_path}/bin/gmg dbupdate",
+#    cwd         => $::mediagoblin::install_path,
+#    environment => "HOME=${::mediagoblin::homedir_path}",
+#    user        => 'mediagoblin',
+#    path        => ['/bin', '/usr/bin'],
+#  }
 }
